328a3a71959d60d3d5d57bb5196ccb1785f0e6bb
moAllElite <mouhamedn347@gmail.com>
2025-06-04 12:52:27 +0000

    Remove Object on ResponseEntity from ApiExceptionHandler

--- src/main/java/com/niangsa/dream_shop/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/niangsa/dream_shop/exceptions/ApiExceptionHandler.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,20 +14,20 @@ import java.time.ZonedDateTime;
 public class ApiExceptionHandler {
 
     @ExceptionHandler({EntityNotFoundException.class})
-    public ResponseEntity<ApiException> handleNoEntityFoundException(EntityNotFoundException e) {
-        ApiException apiException = ApiException.builder().message(e.getMessage())
+    public ResponseEntity<ErrorResponse> handleNoEntityFoundException(EntityNotFoundException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder().message(e.getMessage())
                 .httpStatus(HttpStatus.NOT_FOUND)
                 .timestamp(ZonedDateTime.now(ZoneId.of("Z"))).build();
 
-        return new ResponseEntity<>(apiException,HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({EntityExistsException.class})
-    public ResponseEntity<ApiException> handleEntityAlreadyExistException(EntityExistsException e) {
-        ApiException apiException = ApiException.builder().message(e.getMessage())
+    public ResponseEntity<ErrorResponse> handleEntityAlreadyExistException(EntityExistsException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder().message(e.getMessage())
         .httpStatus(HttpStatus.FOUND).timestamp(ZonedDateTime.now(ZoneId.of("Z"))).build();
         return new  ResponseEntity<>(
-                apiException,
+                errorResponse,
                HttpStatus.FOUND
         );
     }
--- src/main/java/com/niangsa/dream_shop/exceptions/ErrorResponse.java
+++ src/main/java/com/niangsa/dream_shop/exceptions/ErrorResponse.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import java.time.ZonedDateTime;
 
 @Builder
-public record ApiException(
+public record ErrorResponse(
         String message,
         HttpStatus httpStatus,
         ZonedDateTime timestamp
